81bfee718fd7fafbba270bead1b2622cb74b0f5e
Dusan-O <dusan.orescanin@gmail.com>
2022-08-22 01:03:37 +0200

    Initial Commit

---
+++ NetworkProjectApp/NetworkProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkProjectAppApp.swift
+//  NetworkProjectApp
+//
+//  Created by Dusan Orescanin on 22/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
