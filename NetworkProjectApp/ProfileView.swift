6956687baa4d193124ab422b6774ab3a4fe1fcb8
Dusan-O <dusan.orescanin@gmail.com>
2022-08-22 01:26:43 +0200

    Add SF Symbols

--- NetworkProjectApp/ProfileView.swift
+++ NetworkProjectApp/ProfileView.swift
@@ -9,25 +9,47 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        VStack {
-            Image("userPicture1")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 120, height: 120)
-                .clipShape(Circle())
-                .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 5)
-            Text("Alicia")
-                .font(.title2)
-                .fontWeight(.bold)
-            Text("Développeuse iOS")
-                .font(.subheadline)
-                .fontWeight(.light)
+        HStack(alignment: .top) {
+            Button {
+                // When btn pressed
+            } label: {
+                Image(systemName: "bell.badge")
+                    .symbolRenderingMode(.palette)
+                    .font(.system(size: 28))
+                    .foregroundStyle(.red, .gray.opacity(0.5))
+            }
+            Spacer()
+            VStack {
+                Image("userPicture1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 120, height: 120)
+                    .clipShape(Circle())
+                    .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 5)
+                Text("Alicia")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text("Développeuse iOS")
+                    .font(.subheadline)
+                    .fontWeight(.light)
+            }
+            Spacer()
+            Button {
+                // When btn pressed
+            } label: {
+                Image(systemName: "star")
+                    .font(.system(size: 28))
+                    .foregroundColor(.yellow)
+            }
         }
+        .padding()
     }
-}
+
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+            .previewLayout(.sizeThatFits)
     }
+}
 }
